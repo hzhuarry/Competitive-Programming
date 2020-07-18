def4f469eb2c57e94aa5f8d5fd8a10414206e35f
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-07-17 20:15:40 -0400

    fastIO

--- nvim/skeleton.cpp
+++ nvim/skeleton.cpp
@@ -100,8 +100,7 @@ const int dx[4]= {-1,1,0,0}, dy[4]= {0,0,-1,1};
 
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
     cout<<fixed<<setprecision(12);
     //setIO("");
     
