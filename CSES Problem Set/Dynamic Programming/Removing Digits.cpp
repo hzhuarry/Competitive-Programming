8be6f42b307b830a50a7dc19341fcef6fcc97712
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-07-31 15:10:27 -0400

    Create Removing Digits.cpp

---
+++ CSES Problem Set/Dynamic Programming/Removing Digits.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define pb push_back
+using ll = long long;
+
+/* FILE IO */
+void setIO(string filename = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if ((int)filename.size()) { /* USACO */
+        freopen((filename+".in").c_str(), "r", stdin);
+        freopen((filename+".out").c_str(), "w", stdout);
+    }
+}
+
+const ll INF=1e18;
+int n;
+ll dp[1000001];
+
+vector<ll> get_dig(ll x){
+    vector<ll> res;
+    while(x){
+        res.pb(x%10);
+        x/=10;
+    }
+    return res;
+}
+
+int main(){
+    setIO();
+    cin >> n;
+    fill(dp,dp+n+1,INF);
+    dp[n]=0;
+    for(int i=n;i>=0;--i){
+        vector<ll> digits = get_dig(i);
+        for(ll x:digits){
+            if(i-x>=0){
+                dp[i-x]=min(dp[i]+1,dp[i-x]);
+            }
+        }
+    }
+    cout << dp[0] << endl;
+    return 0;
+}
