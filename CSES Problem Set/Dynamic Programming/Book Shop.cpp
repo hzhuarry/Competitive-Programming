7786b1158d09e0a00f4c4c6885517120d8914b65
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-11-20 18:55:02 -0500

    Update Book Shop.cpp

--- CSES Problem Set/Dynamic Programming/Book Shop.cpp
+++ CSES Problem Set/Dynamic Programming/Book Shop.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, x, h[1000], s[1000], dp[1001][100001];
+int n, x, h[1000], s[1000];
+int dp[1001][100001];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -13,14 +14,23 @@ int main() {
     for(int i=0; i<n; ++i)
         cin >> s[i];
 
-    for(int i=1; i<=n; ++i) {
-        for(int cost=1; cost<=x; ++cost) {
-            dp[i][cost] = dp[i-1][cost];
-            if(cost - h[i-1] >= 0) {
-                dp[i][cost] = max(dp[i][cost], dp[i-1][cost - h[i-1]] + s[i-1]);
+    dp[0][0] = 0;
+    for(int i=0; i<n; ++i) {
+        /* @since we are pushing up, we have to modify the dp price from top down */
+        for(int j=x-1; ~j; --j) {
+            dp[i+1][j] = dp[i][j];
+            if(j+h[i]<=x) {
+                /* @ */
+                dp[i+1][j+h[i]] = max(dp[i+1][j+h[i]], dp[i][j]+s[i]);
             }
         }
     }
-    cout << dp[n][x] << endl;
+
+    int res = 0;
+    for(int i=1; i<=x; ++i)
+        res = max(res, dp[n][i]);
+    cout << res;
     return 0;
 }
+
+
