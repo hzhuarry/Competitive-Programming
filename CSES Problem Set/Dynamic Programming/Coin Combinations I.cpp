aa63fd9fb7d3b56fab7e823d31774e6ab248ea75
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-11-20 03:50:29 -0500

    push dp is cleaner

--- CSES Problem Set/Dynamic Programming/Coin Combinations I.cpp
+++ CSES Problem Set/Dynamic Programming/Coin Combinations I.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mod = 1e9 + 7;
-int n, x, c[100], dp[1000001];
+#define ll long long
+
+const ll mod = 1e9 + 7;
+ll n, x, c[100], dp[1000001];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -13,10 +15,10 @@ int main() {
         cin >> c[i];
 
     dp[0] = 1;
-    for(int cost=0; cost<=x; ++cost) {
-        for(int i=0; i<n; ++i) {
-            if(cost - c[i] >= 0) {
-                dp[cost] = (dp[cost] + dp[cost - c[i]]) % mod;
+    for(ll i=0; i<x; ++i) {
+        for(ll j=0; j<n; ++j) {
+            if(i+c[j]<=x) {
+                dp[i+c[j]] = (dp[i+c[j]] + dp[i]) % mod;
             }
         }
     }
