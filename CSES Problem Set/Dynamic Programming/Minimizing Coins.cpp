705e2a501872123ee644a6e98f9c876758f761a8
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-11-20 03:42:25 -0500

    push dp is cleaner

--- CSES Problem Set/Dynamic Programming/Minimizing Coins.cpp
+++ CSES Problem Set/Dynamic Programming/Minimizing Coins.cpp
@@ -1,32 +1,29 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-using ll = long long;
 
-const ll INF=1e18;
-const int maxn=1e6;
+int n, x, c[100], dp[1000001];
 
-int n;
-ll x;
-ll coins[maxn];
-ll dp[maxn+1];
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(){
-    cin.tie(0)->sync_with_stdio(0);
     cin >> n >> x;
-    for(int i=0;i<n;++i)
-        cin >> coins[i];
-    /* 0 coins to have a sum of 0 */
-    for(int i=1;i<=x;++i){
-       dp[i]=INF;
-       for(int j=0;j<n;++j){
-           if(i-coins[j]>=0){
-               dp[i]=min(dp[i],dp[i-coins[j]]+1);
-           }
-       }
+    for(int i=0; i<n; ++i)
+        cin >> c[i];
+
+    memset(dp, 0x3f, sizeof(dp));
+    dp[0] = 0;
+    for(int i=0; i<x; ++i) {
+        for(int j=0; j<n; ++j) {
+            if(i+c[j]<=x)
+                dp[i+c[j]] = min(dp[i+c[j]], dp[i]+1);
+        }
     }
-    ll res=dp[x];
-    if(res==INF) res=-1;
-    cout << res << endl;
+
+    if(dp[x]>=1e9)
+        dp[x] = -1;
+    cout << dp[x];
     return 0;
 }
+
+
