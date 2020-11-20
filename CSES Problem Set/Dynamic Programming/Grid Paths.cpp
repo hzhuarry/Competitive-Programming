e3a5ec76bce584ccc3a03b1c0a2ab5bd1b013ae7
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-11-20 12:55:34 -0500

    cleaner code -> push dp

--- CSES Problem Set/Dynamic Programming/Grid Paths.cpp
+++ CSES Problem Set/Dynamic Programming/Grid Paths.cpp
@@ -1,38 +1,30 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-#define pb push_back
-using ll = long long;
 
-/* FILE IO */
-void setIO(string filename = "") {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    if ((int)filename.size()) { /* USACO */
-        freopen((filename+".in").c_str(), "r", stdin);
-        freopen((filename+".out").c_str(), "w", stdout);
-    }
-}
+const int mod = 1e9 + 7;
+int n, dp[1005][1005];
+char grid[1001][1001];
 
-const ll MOD=1e9+7;
-int n;
-int grid[1001][1001];
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(){
-    setIO();
     cin >> n;
-    for(int i=1;i<=n;++i){
-        string s; cin >> s;
-        for(int j=0;j<n;++j){
-            grid[i][j+1]=s[j]=='.'?1:0;
+    for(int i=1; i<=n; ++i) 
+        for(int j=1; j<=n; ++j)
+            cin >> grid[i][j];
+
+    dp[1][1] = grid[1][1] == '.';
+    for(int i=1; i<=n; ++i) {
+        for(int j=1; j<=n; ++j) {
+            if(grid[i+1][j]!='*')
+                dp[i+1][j] = (dp[i+1][j] + dp[i][j]) % mod;
+            if(grid[i][j+1]!='*')
+                dp[i][j+1] = (dp[i][j+1] + dp[i][j]) % mod;
         }
     }
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=n;++j){
-            if((i==1&&j==1) || grid[i][j]==0) continue;
-            grid[i][j]=grid[i-1][j]+grid[i][j-1];
-            grid[i][j]%=MOD;
-        }
-    }
-    cout << grid[n][n] << endl;
+    cout << dp[n][n];
     return 0;
 }
+
+
