33af5e4e65a17e620e059c31b8ba99312e78661e
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-11-20 19:02:15 -0500

    Update Book Shop.cpp

--- CSES Problem Set/Dynamic Programming/Book Shop.cpp
+++ CSES Problem Set/Dynamic Programming/Book Shop.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int n, x, h[1000], s[1000];
+/* dp[i][x] = maximum number of pages we can get for price at x, only buying among the first i books. */
 int dp[1001][100001];
 
 int main() {
@@ -14,10 +15,13 @@ int main() {
     for(int i=0; i<n; ++i)
         cin >> s[i];
 
+    memset(dp, -1, sizeof(dp));
     dp[0][0] = 0;
     for(int i=0; i<n; ++i) {
         /* @since we are pushing up, we have to modify the dp price from top down */
         for(int j=x-1; ~j; --j) {
+            if(dp[i][j] == -1)
+                continue;
             dp[i+1][j] = dp[i][j];
             if(j+h[i]<=x) {
                 /* @ */
