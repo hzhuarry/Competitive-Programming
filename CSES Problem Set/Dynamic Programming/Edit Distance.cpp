a4485231219e3abef771223dddeeb1dce263c0ba
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-11-22 18:39:45 -0500

    Update Edit Distance.cpp

--- CSES Problem Set/Dynamic Programming/Edit Distance.cpp
+++ CSES Problem Set/Dynamic Programming/Edit Distance.cpp
@@ -1,41 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define endl "\n"
-#define ar array
-typedef long long ll;
-typedef string str;
-
-void setIO(string filename = "") {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    if ((int)filename.size()) {
-        freopen((filename+".in").c_str(), "r", stdin);
-        freopen((filename+".out").c_str(), "w", stdout);
-    }
-}
-
-/* standard Levenshtein distance */
-
-str a,b;
-int dp[5001][5001];
+string s1, s2;
+int dp[5005][5005];
 
 int main() {
-    setIO();
-    cin >> a >> b;
-    int n=(int)a.size(), m =(int)b.size();
-    for(int j=1;j<=m;++j)
-        dp[0][j]=j;
-    for(int i=1;i<=n;++i)
-        dp[i][0]=i;
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=m;++j){
-            if(a[i-1]==b[j-1])
-                dp[i][j] = dp[i-1][j-1];
-            else
-                dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> s1 >> s2;
+
+    memset(dp, 0x3f, sizeof(dp));
+    dp[0][0] = 0;
+    for(int i=0; i<=s1.size(); ++i) {
+        for(int j=0; j<=s2.size(); ++j) {
+            /* delete char s1[i-1] */
+            if(i)
+                dp[i][j] = min(dp[i][j], dp[i-1][j] + 1);
+            /* add char s2[j-1] at the end of s1[i] */
+            if(j)
+                dp[i][j] = min(dp[i][j], dp[i][j-1] + 1);
+            /* replace s1[i-1] with s2[j-1] */
+            if(i and j)
+                dp[i][j] = min(dp[i][j], dp[i-1][j-1] + (s1[i-1] != s2[j-1]));
         }
     }
-    cout << dp[n][m] << endl;
+
+    cout << dp[s1.size()][s2.size()];
     return 0;
 }
+
+
