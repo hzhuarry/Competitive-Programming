5f3e8ac11a1a2a25d4a0390891a8eba2a280fe4d
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2020-12-18 07:10:07 -0500

    Update Shortest Routes II.cpp

--- CSES Problem Set/Graph Algorithms/Shortest Routes II.cpp
+++ CSES Problem Set/Graph Algorithms/Shortest Routes II.cpp
@@ -1,42 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endl "\n"
 #define ll long long
 
-const int MX=500;
-const ll inf = 1e18;
-int n, m, q;
-ll dist[MX][MX];
+ll n, m, q, dist[500][500];
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> m >> q;
-    memset(dist, 0x3f, sizeof(dist));
-    for(int i=0;i<n;++i){
-        dist[i][i] = 0;
+
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            if(i == j) {
+                dist[i][j] = 0;
+            }
+            else {
+                dist[i][j] = (ll)1e18;
+            }
+        }
     }
-    for(int i=0;i<m;++i){
+
+    for(ll i=0; i<m; ++i) {
         ll a, b, c;
         cin >> a >> b >> c;
-        --a, --b;
+        --a;
+        --b;
         dist[a][b] = min(dist[a][b], c);
         dist[b][a] = min(dist[b][a], c);
     }
-    for(int k=0; k<n; ++k){
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n; ++j){
+
+    for(int k=0; k<n; ++k) {
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<n; ++j) {
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
-    while(q--){
-        int a, b;
+
+    while(q--) {
+        ll a, b;
         cin >> a >> b;
-        --a, --b;
-        cout << (dist[a][b]<inf?dist[a][b]:-1) << endl;
+        --a;
+        --b;
+        cout << (dist[a][b] >= (ll)1e12 ? -1 : dist[a][b]) << '\n';
     }
     return 0;
 }
+
+
