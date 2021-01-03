d87c155411e7e9474336c1a89749f8e6040a7e61
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2021-01-02 23:59:11 -0500

    Update Do You Know Your ABCs?.cpp

--- USACO/Contest/2020-2021/Dec/Bronze/Do You Know Your ABCs?.cpp
+++ USACO/Contest/2020-2021/Dec/Bronze/Do You Know Your ABCs?.cpp
@@ -5,16 +5,14 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    long long sum = 0;
-    vector<long long> a(7);
+    vector<int> a(7);
     for(int i=0; i<7; ++i) {
         cin >> a[i];
-        sum += a[i];
     }
         
-    sum /= 4;
     sort(a.begin(), a.end());
-    cout << a[0] << " " << a[1] << " " << sum-(a[0]+a[1]) << '\n';
+    /* a, b, a+b+c-(a+b) */
+    cout << a[0] << " " << a[1] << " " << a.back()-(a[0]+a[1]) << '\n';
     return 0;
 }
 
