d0e610963e6f9bba56c326001e2df6b7e0727ee1
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2021-10-27 22:59:42 -0400

    upd solution

--- USACO/Contest/2015-2016/Open/Silver/closing.cpp
+++ USACO/Contest/2015-2016/Open/Silver/closing.cpp
@@ -1,77 +1,54 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define endl "\n"
-#define pb(s) push_back(s)
-#define mp(a,b) make_pair(a,b)
-#define f first
-#define s second
-#define ALL(v) v.begin(), v.end()
-#define ALLA(arr, sz) arr, arr + sz
-#define SORT(v) sort(ALL(v))
-#define REVERSE(v) reverse(ALL(v))
-#define SORTA(arr, sz) sort(ALLA(arr, sz))
-#define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
-#define PI 3.14159265358979323846264338327950L
-#define MOD 1000000007
+int n, m;
+vector<int> adj[3005];
+bool vis[3005], closed[3005];
 
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-
-int N,M;
-vi adj[3005];
-bool close[3005];
-bool vis[3005];
-int num,cnt;
-
-//find an open farm and dfs
-int open()
-{
-    int x=distance(close,find(ALLA(close+1,N+1),0));
-    return x;
-}
-
-void dfs(int n)
-{
-    vis[n]=1;
-    for(auto u:adj[n]){
-        if(!vis[u]&&!close[u]){
-            dfs(u);
+int dfs(int barn) {
+    vis[barn] = true;
+    int cnt = 1;
+    for(int nextBarn : adj[barn]) {
+        if(!vis[nextBarn] and !closed[nextBarn]) {
+            cnt += dfs(nextBarn);
         }
     }
+    return cnt;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
+int main() {
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ofstream cout ("closing.out");
-    ifstream cin ("closing.in");
+    ifstream cin("closing.in");
+    ofstream cout("closing.out");
 
-    cin>>N>>M;
-    for(int i=0;i<M;++i){
-        int a,b;
-        cin>>a>>b;
-        adj[a].pb(b);
-        adj[b].pb(a);
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i) {
+        int x, y;
+        cin >> x >> y;
+        --x;
+        --y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    num=N;
-    for(int i=0;i<N;++i){
-        int c;
-        cin>>c;
-        if(i==N-1){
-            cout<<"YES"<<endl;
+    for(int cnt = 0; cnt < n; ++cnt) {
+        memset(vis, 0, sizeof(vis));
+        int barn;
+        cin >> barn;
+        --barn;
+        int node = 0;
+        for(int i = 0; i < n; ++i) {
+            if(closed[i])
+                continue;
+            node = i;
             break;
         }
-        for(int j=0;j<3006;++j)vis[j]=0;
-        dfs(open());
-        int x=count(ALLA(vis,3005),1);
-        if(x==num)cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
-        num--;
-        close[c]=1;
+        int open = dfs(node);
+        if(open != n - cnt)
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+
+        closed[barn] = true;
     }
-    return 0;
 }
