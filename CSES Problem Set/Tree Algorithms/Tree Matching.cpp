fa37cbbc3f436887e6112493ca13236ef4a79364
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2021-11-11 18:51:12 -0500

    Update Tree Matching.cpp

--- CSES Problem Set/Tree Algorithms/Tree Matching.cpp
+++ CSES Problem Set/Tree Algorithms/Tree Matching.cpp
@@ -1,52 +1,49 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define endl "\n"
+#define ll long long
 #define ar array
-typedef long long ll;
-typedef string str;
 
-void setIO(string filename = "") {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    if ((int)filename.size()) {
-        freopen((filename+".in").c_str(), "r", stdin);
-        freopen((filename+".out").c_str(), "w", stdout);
-    }
-}
-
-const int maxn=2e5;
 int n;
-vector<int> adj[maxn+1];
-/* 0 = do not include / 1 = include */
-int dp[maxn+1][2];
+vector<int> adj[(int)2e5];
+int dp[(int)2e5][2]; //unused = 0, used = 1,
 
-void dfs(int v=1,int p=-1){
-    for(int u : adj[v]){
-        if(u == p)
+void dfs(int node = 0, int parent = -1) {
+    for(int child: adj[node]) {
+        if(child == parent)
             continue;
-        dfs(u,v);
-        /* if v's edge is not taken, then it is the max between the taken/untaken children */
-        dp[v][0] += max(dp[u][0], dp[u][1]);
+        dfs(child, node);
     }
-    for(int u : adj[v]){
-        if(u == p)
+
+    for(int child: adj[node]) {
+        if(child == parent)
             continue;
-        /* we take the edge to v */
-        dp[v][1] = max(dp[v][1] , dp[u][0]+1+dp[v][0]-max(dp[u][0],dp[u][1]));
-    }    
+        dp[node][0] += dp[child][1];
+    }
+    
+    for(int child: adj[node]) {
+        if(child == parent)
+            continue;
+        dp[node][1] = max(dp[node][1], dp[node][0] - dp[child][1] + dp[child][0] + 1);
+    }
 }
 
 int main() {
-    setIO();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     cin >> n;
-    for(int i=1;i<n;++i){
-        int a,b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+    for(int i = 0; i < n - 1; ++i) {
+        int x, y;
+        cin >> x >> y;
+        --x;
+        --y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
+
     dfs();
-    cout << max(dp[1][0], dp[1][1]) << endl;
-    return 0;
+
+    cout << max(dp[0][0], dp[0][1]);
 }
+
