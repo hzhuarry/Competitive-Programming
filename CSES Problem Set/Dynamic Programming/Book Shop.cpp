47e47bb4c3958776805800a5c73406fed566b444
Harry Zhu <35182194+hzhuarry@users.noreply.github.com>
2022-12-09 01:46:57 -0500

    make clearer

--- CSES Problem Set/Dynamic Programming/Book Shop.cpp
+++ CSES Problem Set/Dynamic Programming/Book Shop.cpp
@@ -1,40 +1,102 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
 using namespace std;
 
-int n, x, h[1000], s[1000];
-/* dp[i][x] = maximum number of pages we can get for price at x, only buying among the first i books. */
-int dp[1001][100001];
+// TEMPLATE {{{
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+/* Macros */
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ar array
+#define sz(x) (int)(x).size()
+#define finish(...) return void(print(__VA_ARGS__))
 
-    cin >> n >> x;
-    for(int i=0; i<n; ++i)
-        cin >> h[i];
-    for(int i=0; i<n; ++i)
-        cin >> s[i];
+/* Input */
+template<class T> void read(T &x) { cin >> x; }
+template<class H, class T> void read(pair<H, T> &p) { cin >> p.first >> p.second; }
+template<class T, size_t S> void read(array<T, S> &a) { for (T &i : a) read(i); }
+template<class T> void read(vector<T> &v) { for (T &i : v) read(i); }
 
-    memset(dp, -1, sizeof(dp));
+template<class H, class... T> void read(H &h, T &...t) { read(h); read(t...); }
+
+/* Output */
+template<class H, class T> ostream &operator<<(ostream &o, pair<H, T> &p) { o << p.first << " " << p.second; return o; }
+template<class T, size_t S> ostream &operator<<(ostream &o, array<T, S> &a) { string s; for (T i : a) o << s << i, s = " "; return o; }
+template<class T> ostream &operator<<(ostream &o, vector<T> &v) { string s; for (T i : v) o << s << i, s = " "; return o; }
+
+template<class H, class T> ostream &operator<<(ostream &o, vector<pair<H, T> > &v) { string s; for (auto& i : v) o << s << i, s = "\n"; return o; }
+template<class T, size_t S> ostream &operator<<(ostream &o, vector<array<T, S> > &v) { string s; for (auto& i : v) o << s << i, s = "\n"; return o; }
+template<class T> ostream &operator<<(ostream &o, vector<vector<T> > &v) { string s; for (auto& i : v) o << s << i, s = "\n"; return o; }
+
+template<class T> void write(T x) { cout << x; }
+template<class H, class... T> void write(const H &h, const T &...t) { write(h); write(t...); }
+
+void print() { write('\n'); }
+template<class H, class... T> void print(const H &h, const T &...t) { write(h); if (sizeof...(t)) write(' '); print(t...); }
+
+/* Debug */
+void DBG() { cerr << "]" << '\n'; }
+template<class H, class... T> void DBG(H h, T... t) { cerr << h; if(sizeof...(t)) cerr << ", "; DBG(t...); }
+#ifdef local
+#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#else
+#define dbg(...) 0
+#endif
+
+template<typename X, typename Y> bool ckmin(X& x, const Y& y) { return (y < x) ? (x = y, 1) : 0; }
+template<typename X, typename Y> bool ckmax(X& x, const Y& y) { return (x < y) ? (x = y, 1) : 0; }
+
+using ll = long long;
+
+// }}}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void run_case() {
+    int n, x;
+    read(n, x);
+    vector<int> price(n), pages(n);
+    read(price, pages);
+
+    vector<vector<int> > dp(n + 1, vector<int>(x + 1, -1));
     dp[0][0] = 0;
-    for(int i=0; i<n; ++i) {
-        /* @since we are pushing up, we have to modify the dp price from top down */
-        for(int j=x-1; ~j; --j) {
-            if(dp[i][j] == -1)
-                continue;
-            dp[i+1][j] = dp[i][j];
-            if(j+h[i]<=x) {
-                /* @ */
-                dp[i+1][j+h[i]] = max(dp[i+1][j+h[i]], dp[i][j]+s[i]);
-            }
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 0; j <= x; ++j) {
+            dp[i][j] = dp[i - 1][j];
+            // we take the book
+            // prev_price = j - price[i - 1]
+            // num of pages = dp[i - 1][prev_price] + pages[i - 1];
+            if(j - price[i - 1] >= 0)
+                ckmax(dp[i][j], dp[i - 1][j - price[i - 1]] + pages[i - 1]);
         }
     }
-
-    int res = 0;
-    for(int i=1; i<=x; ++i)
-        res = max(res, dp[n][i]);
-    cout << res;
-    return 0;
+    print(*max_element(all(dp[n])));
 }
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int t = 1;
+    // read(t);
+    for(int tc = 1; tc <= t; ++tc) {
+        run_case();
+    }
+}
